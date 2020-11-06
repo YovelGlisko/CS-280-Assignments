2c24953f452c29ce140d2e6fb7d6b5afdd9e7102
YovelGlisko <72568312+YovelGlisko@users.noreply.github.com>
2020-11-06 02:52:25 -0500

    Finish the code itself

--- Homework 6/src/Names.java
+++ Homework 6/src/Names.java
@@ -10,6 +10,8 @@ public class Names {
 	public static void main(String[] args) {
 		File boyFile = new File("boynames.txt");
 		File girlFile = new File("girlnames.txt");
+		int numBoys = 0;
+		int numGirls = 0;
 		int falseCount = 0;
 		try {
 			BufferedReader girlInput = new BufferedReader(new FileReader (girlFile));
@@ -17,7 +19,10 @@ public class Names {
 			while (line != null) {
 				String[] split = line.split(" ");
 				names.add(split[0]);
+				numGirls += Integer.parseInt(split[1]);
+				line = girlInput.readLine();
 			}
+			girlInput.close();
 			BufferedReader boyInput = new BufferedReader(new FileReader (boyFile));
 			line = boyInput.readLine();
 			while (line != null) {
@@ -25,12 +30,24 @@ public class Names {
 				if(!add(split[0])) {
 					falseCount++;
 				}
+				numBoys += Integer.parseInt(split[1]);
+				line = boyInput.readLine();
 			}
+			boyInput.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("Error opening file");
 		} catch (IOException e) {
 			System.out.println("Error reading file");
 		}
+		System.out.println("The number of common names is " + commonNames.size());
+		System.out.println("The common names are: ");
+		for(int i = 0; i < commonNames.size(); i++) {
+			System.out.println(commonNames.get(i));
+		}
+		System.out.println("The total number of boys in this census is " + numBoys);
+		System.out.println("The total number of girls in this census is " + numGirls);
+		System.out.println("The total number of individuals in this census is " + (numBoys + numGirls));
+		
 
 	}
 	public static boolean add(String name) {
